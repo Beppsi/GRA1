53b4de174f8f2077b26a0654486de42e2b37908b
Beppsi <46752177+Beppsi@users.noreply.github.com>
2019-03-28 15:43:12 +0100

    .

--- GRA1/PlayGame.c
+++ GRA1/PlayGame.c
@@ -1,8 +1,35 @@
 
+#include <stdio.h>
+#include <string.h>
 
-void PlayGame()
+int main()
 {
-  Word =
-  printf("Welcome, guess a letter:\n");
-  scanf("")
+  char word[10][32]; // 2D array, 32 tecken på 10 rader
+  char buf[32];
+  int index, size, x;
+  FILE *fd; //File descriptor
+  index = 0;
+  fd = fopen("word.txt", "r");
+
+  if (!fd) //Går det att öppna filen?
+  {
+    fprintf(stderr, "no such file\n");
+    return -1;
+  }
+
+  while (fgets(buf, 31, fd)) //fgets, läser filer, loopar så länge det finns text i filen.
+  {
+    size = strlen(buf) -1;
+    buf[size] = 0;
+    strncpy(word[index], buf, 31); //Fyller array med innehåll från .txt
+    index ++;
+  }
+
+  fclose(fd);
+
+  for (i=0); i<index; i++)
+  {
+    printf("%s\n", word[i]);
+    return 0;
+  }
 }
