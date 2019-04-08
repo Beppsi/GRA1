b2d91aaebc57c66f79bf11c1943357040dcce8d8
Beppsi <46752177+Beppsi@users.noreply.github.com>
2019-04-08 12:19:54 +0200

    .

--- GRA1/PlayGame.c
+++ GRA1/PlayGame.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 int main()
 {
-  int index, size, v, i, l, score;
+  int index, size, v, i, l, score, length;
   srand(time(0));
   v = rand() % 10;
   char word[10][32]; //[row] [character]
@@ -16,40 +16,49 @@ int main()
   fd = fopen("word.txt", "r");
 
   if (!fd) //If  word file doesn't exist
-  {
-    fprintf(stderr, "No such file\n");
-    return -1;
-  }
+    {
+      fprintf(stderr, "No such file\n");
+      return -1;
+    }//End of if
 
   while (fgets(buf, 31, fd))
-  {
-    size = strlen(buf) -1;
-    buf[size] = 0;
-    strncpy(word[index], buf, 31); //make array of word.txt
-    index ++;
-  }
+    {
+      size = strlen(buf) -1;
+      buf[size] = 0;
+      strncpy(word[index], buf, 31); //make array of word.txt
+      index ++;
+    }
 
   fclose(fd);
   printf("%d\n", index);
 
     printf("%s\n", word[v]);
-  W = word[v];
+  wo = word[v];
+  lenght = strlen(wo)
 
- for (i=0;i<W.length; i++)
-        {
-         printf("enter letter\n:");
-        scanf("%c", l);
-         if (strcmp(l, W[i]) == 0))
+void compare()
+  {  //compare guess input with letters in word
+    int right = 0; //Start with no right answers
+    for (i=0; i<length; i++);  //For loop goes trough every letter in random word
+      {
+        if (strcmp(guess , word[i]) == 0)
+          {
+            right++; //Adds to right if guess is correct once
+          }//End of if
+
+       if (right > 0) //As long as there is one letter correct
          {
-           printf("correct\n");
-           score ++;
-         }
-         else if (strcmp(l, W[i]) !== 0))
+           printf("Correct");
+         } //End of if
+
+       else  //No letters correct
          {
-           printf("Wrong\n");
-         }
+           printf("Wrong guess");
+         } //End of else
 
       }
+    break;
+  }//End of void compare()
 
- return 0;
-}
+   return 0;
+  }
